626a6dee1bcd84e1e578b5cb771db07cee0cba53
Chinmay Dali <chinmaydali395@gmail.com>
2022-12-17 18:36:52 -0500

    Updates

---
+++ precache-manifest.3764d411c828cc03d163dcf44606049a.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4571666a535edf4fa4a799e45b505cc6",
+    "url": "/Chinmay-395/Portfolio_App_v1/index.html"
+  },
+  {
+    "revision": "25a9cbc974b287f2d89f",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/css/main.639ab11a.chunk.css"
+  },
+  {
+    "revision": "871e111d68105cbb9733",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/js/2.ffd73186.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/js/2.ffd73186.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25a9cbc974b287f2d89f",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/js/main.334a942d.chunk.js"
+  },
+  {
+    "revision": "fd92f0d481f0c732b7fb",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/js/runtime-main.587f270a.js"
+  },
+  {
+    "revision": "63f68ccc85cc2ab7da913353c08cccaf",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/api.63f68ccc.svg"
+  },
+  {
+    "revision": "a42040d5c68d4d3fed9b27878d2fcca5",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/backend.a42040d5.svg"
+  },
+  {
+    "revision": "492c118a49307e4c5b32568ebe67022b",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/blog-app.492c118a.PNG"
+  },
+  {
+    "revision": "524cf16f522b14393c6de355a2eeb145",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/chat-app.524cf16f.png"
+  },
+  {
+    "revision": "083d18ce755dfa9ad6ad57ae17e0058e",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/codesandbox.083d18ce.svg"
+  },
+  {
+    "revision": "ca042db5d0a17f1f6ee36ea90fa2f3a4",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/computer.ca042db5.svg"
+  },
+  {
+    "revision": "cbc38ef18c5a6ecdc0ace14b23e84380",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/github.cbc38ef1.svg"
+  },
+  {
+    "revision": "4ba98a823d931f922f94ff80977e61b0",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/hardware_store.4ba98a82.jpg"
+  },
+  {
+    "revision": "c1eb27404bbc0d6052620ac79194ae19",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/linkedin.c1eb2740.svg"
+  },
+  {
+    "revision": "64c56c72135ba0b4ddf8da186182b723",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/pin.64c56c72.svg"
+  },
+  {
+    "revision": "65fb1478d7ccd11e81f392160777c947",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/portfolio-image.65fb1478.png"
+  },
+  {
+    "revision": "d98bda091e7662d1deacf9fc174577b7",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/portfolio_resume.d98bda09.pdf"
+  },
+  {
+    "revision": "b5fac2be7a6a8edc808f550a24217391",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/puzzle.b5fac2be.svg"
+  },
+  {
+    "revision": "26c488dd1be55304e547e30598fb5ca8",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/python.26c488dd.svg"
+  },
+  {
+    "revision": "2e01c74fe6fcd933e0e9f338e56c0922",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/react.2e01c74f.svg"
+  },
+  {
+    "revision": "1464053c658e62318c689e56c4f1643c",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/repair.1464053c.svg"
+  },
+  {
+    "revision": "e4730e372844c5c90a62067f376c0f4b",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/replit-icon.e4730e37.svg"
+  },
+  {
+    "revision": "9fb5174d107016f5a60a5eefc1eca965",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/sepsis-app.9fb5174d.PNG"
+  },
+  {
+    "revision": "ac89f275383f5f87d437ec63ed4d6031",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/smart-spoon-img.ac89f275.png"
+  },
+  {
+    "revision": "6c469ad28f3769565d6c8b850df57019",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/stackoverflow.6c469ad2.svg"
+  },
+  {
+    "revision": "2854543b3f2d521740c902686ffd49a9",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/taxi-app.2854543b.PNG"
+  },
+  {
+    "revision": "4a04f9f0dd3f59df96497cc7c3021550",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/tie.4a04f9f0.svg"
+  },
+  {
+    "revision": "f46b3d2158c72096c691d9a1bedefb46",
+    "url": "/Chinmay-395/Portfolio_App_v1/static/media/upwork-icon.f46b3d21.svg"
+  }
+]);
